49f1f39e7b59a78a7cb7b7543e68db4f1dc4a4d6
Leks13lebedev <108752679+Leks13lebedev@users.noreply.github.com>
2026-03-24 09:04:14 +0300

    inline.py
    Создали клавиатуру

---
+++ keyboards/inline.py
@@ -0,0 +1,15 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+
+def keyboard(*, btns: dict[str, str], sizes: tuple[int] = (2,)):
+
+    keyboard = InlineKeyboardBuilder()
+
+    for text, data in btns.items():
+        
+        keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
+
+    return keyboard.adjust(*sizes).as_markup()
+
